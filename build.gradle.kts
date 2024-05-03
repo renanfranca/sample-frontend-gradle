29a99a86c54e077d7111d6cbca74d5bd49e113eb
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-05-03 13:14:39 -0300

    Apply jacoco-with-min-coverage-check module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   java
   alias(libs.plugins.spring.boot)
+  jacoco
   // jhipster-needle-gradle-plugins
 }
 
@@ -14,6 +15,43 @@ defaultTasks("bootRun")
 
 springBoot {
   mainClass = "com.mycompany.myapp.JhipsterSampleApplicationApp"
+}
+
+
+jacoco {
+  toolVersion = libs.versions.jacoco.get()
+}
+
+tasks.jacocoTestReport {
+  dependsOn("test", "integrationTest")
+  reports {
+    xml.required.set(true)
+    html.required.set(true)
+  }
+  executionData.setFrom(fileTree(buildDir).include("**/jacoco/test.exec", "**/jacoco/integrationTest.exec"))
+}
+
+tasks.jacocoTestCoverageVerification {
+  dependsOn("jacocoTestReport")
+  violationRules {
+
+      rule {
+          element = "CLASS"
+
+          limit {
+              counter = "LINE"
+              value = "COVEREDRATIO"
+              minimum = "1.00".toBigDecimal()
+          }
+
+          limit {
+              counter = "BRANCH"
+              value = "COVEREDRATIO"
+              minimum = "1.00".toBigDecimal()
+          }
+      }
+  }
+  executionData.setFrom(fileTree(buildDir).include("**/jacoco/test.exec", "**/jacoco/integrationTest.exec"))
 }
 
 // jhipster-needle-gradle-plugins-configurations
@@ -59,6 +97,7 @@ tasks.test {
     excludeTestsMatching("**CucumberTest*")
   }
   useJUnitPlatform()
+  finalizedBy("jacocoTestCoverageVerification")
 }
 
 val integrationTest = task<Test>("integrationTest") {
